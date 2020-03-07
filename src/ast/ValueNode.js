2b13564268fd4d4bb3129364b906f66ce703416c
Jared Barnes <jared.barnes@tcnbroadcasting.com>
2020-03-06 18:45:23 -0700

    Removed comment.

--- src/ast/ValueNode.js
+++ src/ast/ValueNode.js
@@ -1,5 +1,5 @@
 import Node from "./Node.js";
-// We might want reference to the pattern on the node.
+
 export default class ValueNode extends Node {
   constructor(type, name, value, startIndex = 0, endIndex = 0) {
     super(type, name, startIndex, endIndex);

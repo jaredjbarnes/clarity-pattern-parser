8319a752c653e2224609bda19020afe501e4ed9f
Jared Barnes <jared.barnes@tcnbroadcasting.com>
2020-04-04 15:52:29 -0600

    Still writing test.

---
+++ src/tests/CompositeNode.js
@@ -0,0 +1,31 @@
+import CompositeNode from "../ast/CompositeNode.js";
+import assert from "assert";
+import { ValueNode } from "../index.js";
+
+exports["CompositeNode: clone"] = () => {
+  const node = new CompositeNode("type", "name", 0, 0);
+  const valueNode = new ValueNode("value-type", "value-name", "t", 0, 0);
+
+  node.children.push(valueNode);
+
+  const clone = node.clone();
+
+  assert.equal(clone.type, "type");
+  assert.equal(clone.name, "name");
+  assert.equal(clone.children.length, 1);
+  assert.equal(clone.children[0].type, "value-type");
+  assert.equal(clone.children[0].name, "value-name");
+};
+
+exports["CompositeNode: constructor without default indexes."] = () => {
+  const node = new CompositeNode("type", "name");
+  const valueNode = new ValueNode("value-type", "value-name", "t", 0, 0);
+
+  node.children.push(valueNode);
+
+  assert.equal(node.type, "type");
+  assert.equal(node.name, "name");
+  assert.equal(node.children.length, 1);
+  assert.equal(node.children[0].type, "value-type");
+  assert.equal(node.children[0].name, "value-name");
+};
--- src/tests/Node.js
+++ src/tests/Node.js
@@ -3,30 +3,30 @@ import assert from "assert";
 
 exports["Node: clone."] = () => {
   assert.throws(() => {
-    new Node("type", 0, 1).clone();
+    new Node("type", "name", 0, 1).clone();
   });
 };
 
 exports["Node: fitler."] = () => {
   assert.throws(() => {
-    new Node("type", 0, 1).filter();
+    new Node("type", "name", 0, 1).filter();
   });
 };
 
 exports["Node: toString."] = () => {
   assert.throws(() => {
-    new Node("type", 0, 1).toString();
+    new Node("type", "name", 0, 1).toString();
   });
 };
 
 exports["Node: With no startIndex."] = () => {
   assert.throws(() => {
-    new Node("type", undefined, 1);
+    new Node("type", "name", undefined, 1);
   });
 };
 
 exports["Node: With no endIndex."] = () => {
   assert.throws(() => {
-    new Node("type", 0);
+    new Node("type", "name", 0);
   });
 };

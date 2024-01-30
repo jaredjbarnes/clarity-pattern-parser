cbc24bca2b028dfbba06fe83a524632014fb7ff9
Jared Barnes <jared.barnes@tcnbroadcasting.com>
2024-01-30 14:51:35 -0700

    Removed unused import.

--- src/intellisense/Suggestion.ts
+++ src/intellisense/Suggestion.ts
@@ -1,6 +1,5 @@
 import { Node } from "../ast/Node";
 import { Cursor } from "../patterns/Cursor";
-import { Pattern } from "../patterns/Pattern";
 import { SuggestionOption } from "./SuggestionOption";
 
 export interface Suggestion {

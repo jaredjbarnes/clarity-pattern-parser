bfa1ae0ddfa7fd848d32536f4c7637832c653aa9
Jared Barnes <jared.barnes@tcnbroadcasting.com>
2025-10-01 21:57:38 -0600

    Version bump and build

--- dist/intellisense/AutoComplete.d.ts
+++ dist/intellisense/AutoComplete.d.ts
@@ -13,6 +13,12 @@ export interface AutoCompleteOptions {
      * and the string array are the tokens suggested for that pattern.
      */
     customTokens?: Record<string, string[]>;
+    /**
+     * Suggestions may share the same text but differ in their suggestionSequence.
+     * By default, duplicates are removed and only the first instance is kept.
+     * Disabling deduplication allows all distinct instances to be returned together.
+     */
+    disableDedupe?: boolean;
 }
 export declare class AutoComplete {
     private _pattern;
@@ -20,17 +26,29 @@ export declare class AutoComplete {
     private _cursor;
     private _text;
     constructor(pattern: Pattern, options?: AutoCompleteOptions);
+    suggestFor(text: string): Suggestion;
     suggestForWithCursor(cursor: Cursor): Suggestion;
     private getFurthestPosition;
-    suggestFor(text: string): Suggestion;
-    private _getAllOptions;
-    private _getOptionsFromErrors;
-    private _createSuggestionsFromRoot;
-    private _createSuggestionsFromMatch;
-    private _getTokensForPattern;
-    private _getAugmentedTokens;
+    private _getAllSuggestionsOptions;
+    private _createSuggestionOptionsFromErrors;
+    private _createSuggestionOptionsFromMatch;
+    /**
+     * Compares suggestions with provided text and removes completed sub-sequences and preceding text
+     * - IE. **currentText:** *abc*, **sequence:** *[{ab}{cd}{ef}*
+     *   - refines to {d}{ef}
+     */
+    private _trimSuggestionsByExistingText;
+    /** Removed segments already accounted for in the existing text.
+   * ie. sequence pattern segments ≈ [{look}, {an example}, {phrase}]
+   * fullText = "look an"
+   * remove {look} segment as its already been completed by the existing text.
+ */
+    private _filterCompletedSubSegments;
+    private _getCompositeSuggestionsForPattern;
+    private _getCustomTokens;
+    private _deDupeCompositeSuggestions;
     private _createSuggestions;
-    private _createSuggestion;
+    private _createSuggestionOption;
     static suggestFor(text: string, pattern: Pattern, options?: AutoCompleteOptions): Suggestion;
     static suggestForWithCursor(cursor: Cursor, pattern: Pattern, options?: AutoCompleteOptions): Suggestion;
 }
